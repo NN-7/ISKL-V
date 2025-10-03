940a9bd9f8ff09e79dd7c4979f6f5e9b81eabc74
Nadav Nir <73201814+NN-7@users.noreply.github.com>
2025-10-03 16:59:52 +0300

    Made the screenshotter's file sending mechanism work perfectly and made some improvments

--- screenshotter.py
+++ screenshotter.py
@@ -6,38 +6,39 @@ import os
 from contextlib import ExitStack # to close screenshots
 from datetime import datetime, timezone # to classify logs by time sent
 
-URL = 'https://0.0.0.0'
-SCREENSHOT_INTERVAL = 30.0 # Interval between taking screenshots (in seconds).
+URL = 'http://127.0.0.1:8000'
+SCREENSHOT_INTERVAL = 30 # Interval between taking screenshots (in seconds).
 SEND_COUNT = 2 # How many screenshots need to be taken before sending.
+screenshots = []
 # Make sure the screenshot&send count combo don't make the sending interval
 # (screenshot interval * send count) too low so you don't DDOS yourself
 
-screenshots = []
-
 def screenshot():
     with mss.mss() as sct:
-        time = str(datetime.now(timezone.utc).time())[:-7].replace(':','.') # get the current utc time, remove milliseconds portion, and switch colons to periods since you cant use colons in file names
-        screenshot = sct.grab(sct.monitors[0])
+        time = str(datetime.now(timezone.utc))[:-13].replace(':','.') # get the current utc time, remove milliseconds portion, and switch colons to periods since you cant use colons in file names
+        ss = sct.grab(sct.monitors[0])
         screenshot_name = f"screenshot-{time}.png"
-        mss.tools.to_png(screenshot.rgb, screenshot.size, output=screenshot_name)
-        screenshots += screenshot_name
+        mss.tools.to_png(ss.rgb, ss.size, output=screenshot_name)
+        screenshots.append(screenshot_name)
     if len(screenshots) >= SEND_COUNT:
         send_screenshot()
     threading.Timer(SCREENSHOT_INTERVAL, screenshot).start() # make the function run again after the amount of seconds specified in SCREENSHOT_INTERVAL
     
 def send_screenshot():
+    print(screenshots)
     count = len(screenshots) # Assume new screenshots were added which you haven't sent yet and log how many you're sending so you know how many to remove later
     ss_payload = {} # progressively add more screenshots to the payload
     i = 1
     with ExitStack() as stack:
         for ss in screenshots[:count-1]:
             img = stack.enter_context(open(ss, 'rb')) # open the screenshot in read-only mode binary
-            ss_payload[f"screenshot{i}"] = (ss, img) # make the payload for the screenshot containing its name and the screenshot in binary and add it to the list of the payload
+            ss_payload["files"] = (ss, img, 'image/png') # make the payload for the screenshot containing its name, the screenshot in binary, and its MIME type and add it to the list of the payload
             i += 1 # add 1 to the counter so the screenshot keys enumerate ex. {'file1:(..),file2:(..), and so on'}
         r = requests.post(URL, files=ss_payload) # POST (send) the screenshot
-    if not r.ok:
+    if not 300 > r.status_code >= 200:
         pass
     else:
-        screenshots = screenshots[count-1:]
+        del screenshots[:count]
+        print(screenshots)
 
 screenshot()
